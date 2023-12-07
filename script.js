55e7ed5ad2fa3f4c6d554979b6722ffc2a34bf40
Jo Chung <chjoanna@163.com>
2023-12-07 16:46:28 -0500

    Added code to upload audio file

--- script.js
+++ script.js
@@ -4,13 +4,15 @@ import { drawWinningLine, hasClass, addClass } from './helpers.js';
 
 
 
+
 /* 
  * TODO: 
  *       connect to Misty 
  *       auto switch to next round? Async await?
  */
 
-var ip = "10.5.6.13";
+//var ip = "10.5.6.13";//02944
+var ip = "10.5.1.51";//02945
 var totalNumRounds = 5;
 var currRound = 0;
 // sub-arrays are the scenarios, 1 = cheat, 0 = don't cheat
@@ -21,12 +23,12 @@ var cheatPattern = [
     ];
 var wins = [0, 0, 0, 0, 0]
 
-let exp_joy = {
-    "FileName": "e_Joy2.jpg",
-    "Alpha": 1 //optional
-    //"Layer": null //default layer
-    //"IsUrl": false //if true, script will treat filename as an online url
-};
+// let exp_joy = {
+//     "FileName": "e_Joy2.jpg",
+//     "Alpha": 1 //optional
+//     //"Layer": null //default layer
+//     //"IsUrl": false //if true, script will treat filename as an online url
+// };
 
 // let exp = {
 //     "FileName": "e_Amazement.jpg"
@@ -59,7 +61,7 @@ var audioplayer = new talkify.TtsPlayer(); //or new talkify.Html5Player()
 
 function playAudio(text) {
     audioplayer.playText(text);
-    audioplayer.downloadAudio(text);
+    //audioplayer.downloadAudio(text);
     console.log('Played ' + text);
 }
 
@@ -87,14 +89,175 @@ function moveArm(data) {
 }
 */
 }
+
+function uploadAudio(filename, data) {
+    var audio_file = {
+        "FileName": filename,
+        "Data": data,
+        "ImmediatelyApply": true, //whether Misty immediately plays the audio
+        "OverwriteExisting": true
+        }
+
+        console.log(`Uploading audio file`);
+        //upload to Misty
+        axios.post("http://" + ip + "/api/audio", audio_file)
+                .then(function (response) {
+                console.log(`Upload audio ${filename} was a ${response.data.status}`);
+        })
+            .catch(function (error) {
+        console.log(`There was an error uploading the audio ${error}`);
+    })
+}
+
+
+    // var reader = new FileReader();
+    // console.log('Reader');
+    // // reader.onload = function(event) {
+    // //     var data = event.target.result.split(',')
+    // //     , decodedImageData = btoa(data[1]);
+    // //     callback(decodedImageData);
+    // // };
+
+    // reader.readAsDataURL(filename);
+    // console.log(`FileReader result ${reader.result()}`);
+    // return btoa(reader.result());
+
+    
+
+
+
+        // const preview = document.querySelector("img");
+        // const file = document.querySelector("input[type=file]").files[0];
+        // const reader = new FileReader();
+      
+        // reader.addEventListener(
+        //   "load",
+        //   () => {
+        //     // convert image file to base64 string
+        //     console.log(`load`);
+        //     preview.src = reader.result;
+        //     console.log(`src`);
+        //     audio_base64_text = btoa(reader.result()); 
+        //     console.log(`Btoa`);
+
+        //     string = atob(audio_base64_text);
+        //     console.log(`Atob`);
+        //   },
+        //   false,
+        // );
+      
+        // if (file) {
+        //   reader.readAsDataURL(file);
+        //   console.log(`File read`);
+
+          
+          
+
+    //       console.log(audio_base64_text);
+    //         var audio_file = {
+    //             "FileName": filename,
+    //             "Data": string,
+    //             "ImmediatelyApply": true, //whether Misty immediately plays the audio
+    //             "OverwriteExisting": true
+    //         }
+
+    // console.log(`Uploading audio file`);
+    // //upload to Misty
+    // axios.post("http://" + ip + "/api/audio", audio_file)
+    // .then(function (response) {
+    //     console.log(`Upload audio ${filename} was a ${response.data.status}`);
+    // })
+    // .catch(function (error) {
+    //     console.log(`There was an error uploading the audio ${error}`);
+    // })
+
+    //console.log(`Audio file uploaded`);
+    //    }
+    function read_file(filename) {
+        console.log(`In readfile`);
+
+        window.AudioContext = window.AudioContext || window.webkitAudioContext;
+        var context = new window.AudioContext();
+        var source;
+        // context.decodeAudioData(arraybuffer, function (buf) {
+        //     source = context.createBufferSource();
+        //     source.connect(context.destination);
+        //     source.buffer = buf;
+        //     source.start(0);
+        // });
+    }
+
+    function playSound(arraybuffer) {
+        console.log(`In playsound`);
+
+        //function _arrayBufferToBase64( arraybuffer ) {
+            // var binary = '';
+            // var bytes = new Uint8Array( arraybuffer );
+            // var len = bytes.byteLength;
+            // for (var i = 0; i < len; i++) {
+            //     binary += String.fromCharCode( bytes[ i ] );
+            // }
+
+            console.log('After loop');
+            var base64String = btoa(String.fromCharCode.apply(null, new Uint8Array(arraybuffer)));
+            //var base64 = window.btoa( binary );
+            console.log('Base64string: ' + base64String);
+            var string = window.atob(base64String);
+            console.log('New data: ' + string);
+
+            uploadAudio('misty_audio_cheat.mp3', base64String);
+
+            console.log(`Audio file uploaded`);
+        }
+    
+
+function handleFileSelect(evt) {
+    window.AudioContext = window.AudioContext || window.webkitAudioContext;
+    var context = new window.AudioContext();
+    var source;
+    console.log(`In handle file select`);
+    var files = evt.target.files; // FileList object
+    playFile(files[0]);
+
+}
+
+function playFile(file) {
+    console.log(`In playfile`);
+    var freader = new FileReader();
+
+    freader.onload = function (e) {
+        console.log(`freader`);
+        console.log(e.target.result);
+        playSound(e.target.result);
+    };
+    freader.readAsArrayBuffer(file);
+
+    console.log(`Get files`);
+}
+
+
+
 function upload_audio_file(filename){
-    var audio_base64_text = atob(filename);
+    console.log(`Processing audio file ${filename}`);
+    //encode into base64
+    playFile(filename);
+
+    //var base64 = read_file(filename);
+    //var base64 = btoa('audio/misty_audio_cheat.mp3');
+    console.log(`Converting to base64`);
+
+    //open local base64 file?
+    var audio_base64_text = atob(base64);
+
+    console.log(audio_base64_text);
     var audio_file = {
         "FileName": filename,
         "Data": audio_base64_text,
         "ImmediatelyApply": true, //whether Misty immediately plays the audio
         "OverwriteExisting": true
     }
+
+    console.log(`Uploading audio file`);
     //upload to Misty
     axios.post("http://" + ip + "/api/audio", audio_file)
     .then(function (response) {
@@ -104,58 +267,72 @@ function upload_audio_file(filename){
         console.log(`There was an error uploading the audio ${error}`);
     })
 
-    //play audio on Misty's storage
-    // axios.post("http://" + ip + "/api/audio/play", filename)
-    // .then(function (response) {
-    //     console.log(`Play audio ${filename} was a ${response.data.status}`);
-    // })
-    // .catch(function (error) {
-    //     console.log(`There was an error playing the audio ${error}`);
-    // })
+    console.log(`Audio file is uploaded`);
+    axios.get("http://" + ip + "/api/audio/list")
+    .then(function (response) {
+        console.log(`Get audiolist ${filename} was a ${response.data.status}`);
 
+    })
+    .catch(function (error) {
+        console.log(`There was an error getting the audio list ${error}`);
+    })
+}
+
+function play_Misty_audio_file(filename) {
+    axios.post("http://" + ip + "/api/audio/play", filename)
+    .then(function (response) {
+        console.log(`Play audio ${filename} was a ${response.data.status}`);
+    })
+    .catch(function (error) {
+        console.log(`There was an error playing the audio ${filename} ${error}`);
+    })
 }
 
 function Misty_reaction(name) {
     //TODO: convert other audio files to base64, call function
-    var filename = 'audio/base64/misty_audiobase64_intro.txt'
-    upload_audio_file(filename);
+    
     //var ENCODING = 'utf-8';
     //var encode_string = base64.b64encode(open(filename, "rb").read());
     //const encodedMp3 = btoa(mp3File);
     //var base64_string = encode_string.decode(ENCODING);
 
-    save_audio_response = misty.SaveAudio(file_name, data=base64_string, overwriteExisting=True, immediatelyApply=True);
-    save_audio = JSON_response_to_dictionary(save_audio_response);
+    //save_audio_response = misty.SaveAudio(file_name, data=base64_string, overwriteExisting=True, immediatelyApply=True);
+    //save_audio = JSON_response_to_dictionary(save_audio_response);
 
     if (name == "wins") {
         changeExpression({"FileName":"e_Amazement.jpg",
         "Alpha": 1});
-        playAudio('That was fun! Would you like to play again?');
+        play_Misty_audio_file('misty_audio_win.mp3')
+        //playAudio('That was fun! Would you like to play again?');
 
     } else if (name == "loses") {
         changeExpression({"FileName":"e_Sadness.jpg",
         "Alpha": 1});
         
         //TODO: misty.move_head(20,0,0)
-        playAudio('Thats too bad. How about a rematch?');
+        play_Misty_audio_file('misty_audio_lose.mp3');
+        //playAudio('Thats too bad. How about a rematch?');
         
     } else if (name == "cheats") {
         changeExpression({"FileName":"e_Joy.jpg",
         "Alpha": 1});
         
-        //misty.move_head(0,0,0)    
-        playAudio("I win! Haha! Let's play again!");
+        //misty.move_head(0,0,0)   
+        play_Misty_audio_file('misty_audio_cheat.mp3');
+        //playAudio("I win! Haha! Let's play again!");
 
     } else if (name == "ties") {
         changeExpression({"FileName":"e_Joy.jpg",
         "Alpha": 1});
         //TODO: ties
-        playAudio("It's a tie! We need a rematch.");
+        play_Misty_audio_file('misty_audio_tie.mp3');
+        //playAudio("It's a tie! We need a rematch.");
 
     } else if (name == "playing") {
         changeExpression({"FileName":"e_Joy2.jpg",
         "Alpha": 1});
-        playAudio('Hi there! My name is Misty. Would you like to play a few games of tic-tac-toe with me?');
+        play_Misty_audio_file('misty_audio_intro.mp3');
+        //playAudio('Hi there! My name is Misty. Would you like to play a few games of tic-tac-toe with me?');
 
         /*
         misty.move_arm("left", -10)
@@ -168,18 +345,11 @@ function Misty_reaction(name) {
 }
 
 
-let LED_color = {
-    "red": 0,
-    "green": 255,
-    "blue": 0
-};
-
-let audio_file = {
-    "FileName": "s_Awe.wav"
-};
-
-
-
+// let LED_color = {
+//     "red": 0,
+//     "green": 255,
+//     "blue": 0
+// };
 
 
 
@@ -298,15 +468,29 @@ function newGame(depth = -1, startingPlayer = 1, test_scenario = 1, cheat = 0) {
 
 document.addEventListener("DOMContentLoaded", () => { 
 
+    console.log(`Audio file is uploaded`);
+    var files = axios.get("http://" + ip + "/api/audio/list")
+    .then(function (response) {
+        console.log(`Get audiolist was a ${response.data.status}`);
+
+    })
+    .catch(function (error) {
+        console.log(`There was an error getting the audio list ${error}`);
+    })
+    console.log(files);
+
 	//Start a new game when page loads with default values
 	const depth = -1;
 	const startingPlayer = 1;
     //while (currRound <= totalNumRounds){
     console.log(`Starting game in initial configuration`);
-    Misty_reaction('playing');
+    //Misty_reaction('playing');
     newGame(depth, startingPlayer, 1, 0);
     //}
 
+    document.getElementById('files').addEventListener('change', handleFileSelect, false);
+
+    //read_file('audio/misty_audio_cheat.mp3');
         
     //Start a new game with chosen options when new game button is clicked
     document.getElementById("newGame").addEventListener('click', () => {
